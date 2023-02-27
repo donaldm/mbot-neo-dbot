74d572ab3683b234b0c6d629c26343cb0df419ae
Donald May <donaldmay@acm.org>
2023-02-26 23:17:04 -0600

    Test open and close gripper

---
+++ dbot-intent-service/tests/test_open_gripper_commands.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pytest
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from dbot_intent_service import DBotIntentParser
+
+@pytest.fixture
+def dbot_intent_parser_fixture():
+    dbot_intents_file_path = os.path.join(parent, 'dbot_intents.yaml')
+    dbot_intent_parser = DBotIntentParser(dbot_intents_file_path, parent)
+    return dbot_intent_parser
+
+@pytest.mark.open_gripper_commands
+@pytest.mark.parametrize('phrase', ['open gripper', 'open hand'])
+def test_open_gripper(dbot_intent_parser_fixture, phrase):
+    intent = dbot_intent_parser_fixture.calc_intent(f'{phrase}'.format(phrase=phrase))
+    assert intent.name == 'open_gripper'
+    assert intent.conf > 0.5
