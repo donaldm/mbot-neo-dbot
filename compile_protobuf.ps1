88d680720e757ffc79dc705d31b6dfac2c0d0af4
Donald May <donaldmay@acm.org>
2023-03-01 22:46:27 -0600

    Fix path

--- compile_protobuf.ps1
+++ compile_protobuf.ps1
@@ -1,11 +1,14 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
 $folders = "messages", "dbot-intent-service/messages"
+$protobufs = "DBotCommand.proto", "DBotStatus.proto", "DBotIntent.proto"
 
 foreach ($folder in $folders)
 {
     New-Item -Path $folder -ItemType Directory -Force
-    protoc -I=protobufs --python_out=$folder protobufs\DBotCommand.proto
-    protoc -I=protobufs --python_out=$folder protobufs\DBotStatus.proto
-    protoc -I=protobufs --python_out=$folder protobufs\DBotIntent.proto
+    foreach ($protobuf in $protobufs)
+    {
+        $protobufPath = Join-Path -Path 'protobufs' -ChildPath $protobuf
+        protoc -I=protobufs --python_out =$folder protobufs\$protobufPath
+    }
 }
